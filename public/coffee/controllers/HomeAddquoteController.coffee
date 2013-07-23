560ed25021cddd5e5b3f44bef82333f312719127
steve kane <skane@4-south.com>
2013-07-22 23:38:13 -0500

    added minor cosmetic tweaks to clientsheader sass file.  corrected clients bug in addquote modal

--- public/coffee/controllers/HomeAddquoteController.coffee
+++ public/coffee/controllers/HomeAddquoteController.coffee
@@ -2,6 +2,7 @@ alias = Ember.computed.alias
 
 AR.HomeAddquoteController = Ember.Controller.extend
   
-  needs: ['insurancetypes']
+  needs: ['insurancetypes', 'clients']
   
+  clients: alias "controllers.clients.content"
   insuranceTypes: alias "controllers.insurancetypes.content"
--- public/dist/agentrun.js
+++ public/dist/agentrun.js
@@ -57,7 +57,8 @@ var alias;
 alias = Ember.computed.alias;
 
 AR.HomeAddquoteController = Ember.Controller.extend({
-  needs: ['insurancetypes'],
+  needs: ['insurancetypes', 'clients'],
+  clients: alias("controllers.clients.content"),
   insuranceTypes: alias("controllers.insurancetypes.content")
 });
 
